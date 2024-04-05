acd5d75450e2ac031da48fa1fb13dbc74336730c
ByteWire3D <165397383+ByteWire3D@users.noreply.github.com>
2024-04-05 11:19:39 +0200

    add comments mac_adress.ino

--- Files/mac_adress.ino
+++ Files/mac_adress.ino
@@ -9,7 +9,7 @@ void setup(){
   Serial.println();
   while(Serial != true){delay(10);}
   Serial.print("ESP Board MAC Address:  ");
-  Serial.println(WiFi.macAddress());
+  Serial.println(WiFi.macAddress());//get mac adress
 }
  
 void loop(){

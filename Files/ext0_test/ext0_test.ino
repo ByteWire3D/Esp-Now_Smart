9e7356f94a6280edf1d62ef16883ec485f08fed2
ByteWire3D <165397383+ByteWire3D@users.noreply.github.com>
2024-05-28 22:32:55 +0200

    Create ext0_test.ino

---
+++ Files/ext0_test/ext0_test.ino
@@ -0,0 +1,60 @@
+RTC_DATA_ATTR int bootCount = 0;
+
+/*
+  Method to print the reason by which ESP32
+  has been awaken from sleep
+*/
+void print_wakeup_reason() {
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  Serial.println("");
+  Serial.println("");
+  Serial.println("");
+  Serial.println("EXT0 Test");
+
+  switch (wakeup_reason)
+  {
+    case 1  : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case 2  : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case 3  : Serial.println("Wakeup caused by timer"); break;
+    case 4  : Serial.println("Wakeup caused by touchpad"); break;
+    case 5  : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.println("Wakeup was not caused by deep sleep"); break;
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000); //Take some time to open up the Serial Monitor
+
+  //Increment boot number and print it every reboot
+  ++bootCount;
+  Serial.println("Boot number: " + String(bootCount));
+
+  //Print the wakeup reason for ESP32
+  print_wakeup_reason();
+
+  /*
+    First we configure the wake up source
+    We set our ESP32 to wake up for an external trigger.
+    There are two types for ESP32, ext0 and ext1 .
+    ext0 uses RTC_IO to wakeup thus requires RTC peripherals
+    to be on while ext1 uses RTC Controller so doesnt need
+    peripherals to be powered on.
+    Note that using internal pullups/pulldowns also requires
+    RTC peripherals to be turned on.
+  */
+  Serial.println(esp_sleep_enable_ext0_wakeup(GPIO_NUM_39, 1)); //1 = High, 0 = Low
+
+  //Go to sleep now
+  Serial.println("Going to sleep now");
+  Serial.flush();
+  esp_deep_sleep_start();
+  Serial.println("This will never be printed");
+}
+
+void loop() {
+  //This is not going to be called
+}

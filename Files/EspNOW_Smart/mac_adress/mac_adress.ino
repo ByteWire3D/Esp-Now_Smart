181948e55e1ae75c5b68f882adf27a7146954c6d
ByteWire3D <165397383+ByteWire3D@users.noreply.github.com>
2024-05-30 22:11:00 +0200

    Create mac_adress.ino

---
+++ Files/EspNOW_Smart/mac_adress/mac_adress.ino
@@ -0,0 +1,17 @@
+#ifdef ESP32
+  #include <WiFi.h>
+#else
+  #include <ESP8266WiFi.h>
+#endif
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println();
+  while(Serial != true){delay(10);}
+  Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+}
+ 
+void loop(){
+
+}
